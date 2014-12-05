e96e9b2a9fbad2a0907eb78c29ff63cb27808456
dan brooks <dan@danbrooks.net>
2014-12-05 17:32:53 -0500

    adds light controls

--- cameractrl.py
+++ cameractrl.py
@@ -26,19 +26,22 @@ from twisted.internet.protocol import ReconnectingClientFactory
 from twisted.protocols.basic import LineReceiver
 
 
+#TODO: Turn on and off security light
+
 def log(msg, priority=syslog.LOG_INFO):
     """ Prints a message and also saves it to the syslog """
     print msg
     syslog.syslog(priority, msg)
 
 class MochadListener(LineReceiver):
-    """ Receives messages from mochad server and logs the data """
+    """ Listens to mochad sever for signals to control Motion webcam and security light """
     delimiter = '\n'
 
-    def __init__(self, mochad_code, start_command, stop_command):
+    def __init__(self, mochad_inputcode, start_command, stop_command, mochad_lightcode):
         self.__pattern = "(?:\d{2}/\d{2}) (?:\d{2}:\d{2}:\d{2}) (?:Rx|Tx) (?:RF|RFSEC|PL) (?:[a-zA-Z]+:) (\S+) Func: (\S+)"
-        self.__code = mochad_code
-        self.__commands = {"On": start_command, "Off": stop_command}
+        self.__inputcode = mochad_inputcode
+        self.__motioncommands = {"On": start_command, "Off": stop_command}
+        self.__lightcode = mochad_lightcode
 
     def connectionMade(self):
         log("Connected!")
@@ -50,25 +53,31 @@ class MochadListener(LineReceiver):
             log("Could not parse input '%s'. Aborting." % line, syslog.LOG_ALERT)
             return
 
-        # Ignore if this is not the code we are looking for
-        if not self.__code == input_code:
+        # Ignore if this is not the input code we are looking for
+        if not self.__inputcode == input_code:
             log("wrong device, ignoring.", syslog.LOG_INFO)
             return
 
-        # Create command - commands with arguments need put into a list
-        if not input_cmd in self.__commands:
+        # Ignore if we do not understand the command
+        if not input_cmd in self.__motioncommands:
             log("Command '%s' not accepted. Supported commands: %s. Aborting." % 
-                (input_cmd, ",".join(self.__commands.keys())),
+                (input_cmd, ",".join(self.__motioncommands.keys())),
                 syslog.LOG_ALERT)
             return
 
-        cmd = self.__commands[input_cmd]
-        log("calling '%s'"  % cmd)
+        # Execute camera command. Commands with arguments need to be put into a list
+        cmd = self.__motioncommands[input_cmd]
         cmd = cmd.split() if ' ' in cmd else cmd
         try:
             ret = subprocess.call(cmd, stdout=open("/dev/null", "w"))
+            log("executing '%s'"  % self.__motioncommands[input_cmd], syslog.LOG_DEBUG)
         except Exception:
             log("Command Failed", syslog.LOG_ALERT)
+
+        # Execute Light Command
+        cmd = "rf %s %s" % (self.__lightcode, input_cmd.lower())
+        log("Sending mochad '%s'" % cmd, syslog.LOG_DEBUG)
+        self.sendLine(cmd)
         return
 
 class ConnectionManager(ReconnectingClientFactory):
@@ -96,12 +105,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("cameractrl", description="Motion Camera Control")
     parser.add_argument('--host', type=str, required=True, help="ip address of mochad server")
     parser.add_argument('--port', type=int, default=1099, help="mochad listening port")
-    parser.add_argument('--code', type=str, default="C1", help="mochad device code to listen to for input signal")
+    parser.add_argument('--inputcode', type=str, default="C1", help="mochad device code to listen to for input signal")
     parser.add_argument('--motion-start-cmd', default='/etc/init.d/motion start', help="motion start command")
     parser.add_argument('--motion-stop-cmd', default='/etc/init.d/motion stop', help="motion stop command")
+    parser.add_argument('--camera-light-code', default='m5', help="X10 device code for camera infrared light")
     args = parser.parse_args()
 
-    listener = MochadListener(args.code, args.motion_start_cmd, args.motion_stop_cmd)
+    listener = MochadListener(args.inputcode, 
+                              args.motion_start_cmd, 
+                              args.motion_stop_cmd, 
+                              args.camera_light_code)
+    
     connection_manager = ConnectionManager(listener)
     reactor.connectTCP(args.host, args.port, connection_manager)
     reactor.run()
